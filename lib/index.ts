fe7d3f1e4008471a07da306bfa035100ec81640a
Amandeep Kirar <ak4wrapp@gmail.com>
2025-02-22 21:51:26 -0500

    export utils and tgz too

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,11 @@ import MyButton from "./components/MyButton/my-button";
 import BeautifulTimeline from "./components/Timeline";
 import BeautifulTimelineItem from "./components/TimelineItem";
 import "./index.css";
+import { divedeHalfNumberValueInString } from "./utils";
 
-export { BeautifulTimeline, BeautifulTimelineItem, MyButton };
+export {
+  BeautifulTimeline,
+  BeautifulTimelineItem,
+  MyButton,
+  divedeHalfNumberValueInString,
+};
--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,3 +1,4 @@
+// This function divides the provided value by 2 and return the value in string format
 export const divedeHalfNumberValueInString = (value: string) => {
   const numericValue = value ? parseFloat(value.replace(/[^0-9.-]+/g, "")) : 0;
   return (
